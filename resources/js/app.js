cb54bd5e5fe0042b5598476b9bf16235fffeb5cb
Ronald Vincent Bustillo <rbustillo@legacyselller.com>
2021-05-15 21:44:49 +0800

    for abing

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,22 +6,32 @@ import swal from 'admin-lte/plugins/sweetalert2/sweetalert2.min.js'
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
 import VueRouter from 'vue-router'
+import VueAlertify from 'vue-alertify';
 Vue.use(toastr)
 Vue.use(Loading)
 Vue.use(VueRouter)
+Vue.use(VueAlertify,{
+    movable: false,
+    glossary: {
+      title: 'Confirm',
+      ok: 'OK',
+      cancel: 'Cancel',
+    },
+  });
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('login',require('./components/Login.vue').default);
 Vue.component('register',require('./components/Register.vue').default);
 Vue.component('vendors',require('./components/Vendor.vue').default);
 Vue.component('home',require('./components/Home.vue').default);
-
+Vue.component('home',require('./components/Dashboard.vue').default);
 window.swal = swal;
 window.toastr = require('toastr')
 
 const routes = [
     { path: '/vendors', component: require('./components/Vendor.vue').default },
     { path: '/home', component: require('./components/Home.vue').default },
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
   ]
 
 const router = new VueRouter({
