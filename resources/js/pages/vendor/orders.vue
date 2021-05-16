088095a2a7654dd4b5c4e92e289c2d152310e3f2
AbingPj <pj.abing@gmail.com>
2021-05-16 13:25:12 +0800

    Added Vendor Orders Table

--- resources/js/pages/vendor/orders.vue
+++ resources/js/pages/vendor/orders.vue
@@ -17,14 +17,105 @@
 		</div>
 		<section class="content">
 			<div class="container-fluid">
-                  contents here
-            </div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="card card-primary card-outline">
+							<div class="card-header">
+								<h3 class="card-title">Users List</h3>
+							</div>
+							<div class="card-body table-responsive">
+								<table
+									id="mytable"
+									class="table table-bordered table-striped dt-responsive display nowrap"
+								>
+									<thead>
+										<tr>
+											<th>Order Id</th>
+											<th>Amount</th>
+											<th>Status</th>
+											<th>Date</th>
+											<th>Action</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr v-for="(item, index) in orders" :key="index">
+											<td>{{ item.id }}</td>
+											<td>
+												<b
+													>₱
+													{{
+														addCommaAndTwoDecimalPlaces(
+															item.total_price
+														)
+													}}</b
+												>
+											</td>
+											<td>
+												<span
+													v-if="item.status === 1"
+													class="badge badge-success"
+													>{{ item.status_label }}</span
+												>
+												<span v-else class="badge badge-warning">{{
+													item.status_label
+												}}</span>
+											</td>
+											<td>{{ item.date }}</td>
+											<td>
+												<router-link
+													class="btn btn-info btn-sm"
+													:to="'vendor/orders/' + item.id"
+													>Details</router-link
+												>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: "vendor orders",
+		data() {
+			return {
+				orders: [
+					{
+						id: 1,
+						total_price: 1500,
+						status: 1,
+						status_label: "Success",
+						date: "May 5, 2021",
+					},
+				],
+			};
+		},
+		mounted() {
+			setTimeout(() => {
+				this.myTable();
+			}, 1000);
+		},
+		methods: {
+			myTable() {
+				$(document).ready(function () {
+					$("#mytable").DataTable({
+						responsive: true,
+					});
+				});
+			},
+			addCommaAndTwoDecimalPlaces(value) {
+				var n = parseFloat(value).toFixed(2);
+				var withCommas = Number(n).toLocaleString("en", {
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 2,
+				});
+				return withCommas;
+			},
+		},
 	};
 </script>
