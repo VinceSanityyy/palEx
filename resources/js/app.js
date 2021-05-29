df0355dd3d443ad53884a01f5f3edbb93caaf3fb
AbingPj <pj.abing@gmail.com>
2021-05-29 12:46:35 +0800

    Added element UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,10 @@ import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
 import VueRouter from 'vue-router'
 import VueAlertify from 'vue-alertify';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
 Vue.use(toastr)
 Vue.use(Loading)
 Vue.use(VueRouter)
