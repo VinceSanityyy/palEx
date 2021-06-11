e36304310b1fa1686cc326e3d2dc397d71811fbe
Ronald Vincent Bustillo <rbustillo@legacyselller.com>
2021-06-11 12:28:44 +0800

    products ongoing

--- resources/js/pages/vendor/products.vue
+++ resources/js/pages/vendor/products.vue
@@ -21,7 +21,8 @@
 					<div class="col-md-12">
 						<div class="card card-primary card-outline">
 							<div class="card-header">
-								<h3 class="card-title">Products List</h3>
+								<h3 class="card-title" >Products List</h3>
+								<button data-toggle="modal" data-target="#exampleModal" class="btn btn-primary" style="float:right">Add New Product</button>
 							</div>
 							<div class="card-body table-responsive">
 								<table
@@ -78,6 +79,43 @@
 				</div>
 			</div>
 		</section>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+		<div class="form-group">
+			<label >Product Name</label>
+			<input v-model="product_name" type="text" class="form-control">
+			<label >Price</label>
+			<input v-model="price" type="text" class="form-control">
+			<label >Unit</label>
+			<input v-model="unit" type="text" class="form-control">
+			<label >Category</label>
+			    <select v-model="category" class="form-control" id="exampleFormControlSelect2">
+				<option value="1">Wet</option>
+				<option value="2">Dry</option>
+				<option value="3">Others</option>
+
+				</select>
+			<label for="exampleFormControlFile1">Image</label>
+    		<input type="file" class="form-control-file" id="exampleFormControlFile1">
+		</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal -->
 	</div>
 </template>
 
@@ -97,6 +135,10 @@
 						product_status: "Active",
 					},
 				],
+				product_name:'',
+				price:'',
+				unit:'',
+				category:''
 			};
 		},
         mounted() {
