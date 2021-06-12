2595ed4192dba24794c435032b589b9cebd456c8
AbingPj <pj.abing@gmail.com>
2021-06-12 14:31:12 +0800

    Added more seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -34,6 +34,27 @@ class UserSeeder extends Seeder
                 'password' => \Hash::make('123123123'),
                 'role' => 1,
                 'status' => 1
+            ],
+            [
+                'name' => 'Admin Pj',
+                'email' => 'admin.pj@gmail.com',
+                'password' => \Hash::make('password'),
+                'role' => 0,
+                'status' => 1
+            ],
+            [
+                'name' => 'Abing Pj',
+                'email' => 'abing.pj@gmail.com',
+                'password' => \Hash::make('password'),
+                'role' => 1,
+                'status' => 1
+            ],
+            [
+                'name' => 'Pj Abing',
+                'email' => 'pj.abing@gmail.com',
+                'password' => \Hash::make('password'),
+                'role' => 2,
+                'status' => 1
             ]
         ];
         \DB::table('users')->insert($data);
