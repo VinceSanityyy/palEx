c891d4e532bf0f38f930399a6b8ffff47a38e209
AbingPj <pj.abing@gmail.com>
2021-07-10 19:53:50 +0800

    Applied Get Cart APi to the frontend

--- resources/js/pages/frontside/customer/cart.vue
+++ resources/js/pages/frontside/customer/cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="content mt-5" style="margin-bottom:5rem;">
+  <section class="content mt-5" style="margin-bottom: 5rem">
     <div v-if="cartList.length <= 0" class="container palex-checkout-page">
       <div class="row">
         <div class="col-md-12">
@@ -14,115 +14,41 @@
     <div v-if="cartList.length >= 1" class="container palex-checkout-page">
       <div class="row">
         <div class="col-lg-8 col-md-7">
-          <div class="palex-card">
+          <div v-for="(item, index) in cartList" :key="index" class="palex-card mb-2">
             <div class="w-100">
               <div class="store-name ml-3 mb-2 p-2 border-bottom border-secondary">
-                <span> <i class="fas fa-store mr-2"></i> Store 1</span>
+                <span> <i class="fas fa-store mr-2"></i> {{ item.vendor.name }} </span>
                 <!-- <el-divider><i class="el-icon-star-on"></i></el-divider> -->
               </div>
-              <div class="item border-bottom border-success p-2">
+              <div v-for="(el, index2) in item.items" :key="index2" class="item border-bottom border-success p-2">
                 <div class="d-flex">
                   <!-- <div class="check-box mx-3" style="display: flex; align-items: center; height: auto">
                     <el-button type="danger" icon="el-icon-delete" circle size="mini"></el-button>
                   </div> -->
                   <div class="prod-img" style="min-width: 90px">
-                    <img style="width: 80px; height: 80px; object-fit: cover" src="/storage/images/products/3jaCqHANMkXd6F2sNpfxGtmVEtqTBWXq1HNke1k4.jpg" alt="" />
+                    <img style="width: 80px; height: 80px; object-fit: cover" :src="el.product.image_link" alt="" />
                   </div>
                   <div class="prod-name w-100">
                     <div class="d-flex">
                       <div>
-                        <span>Product Name </span>
+                        <span>{{ el.product.name }}</span>
                       </div>
                       <div class="ml-2 ml-auto">
                         <el-button type="danger" icon="el-icon-delete" circle size="mini"></el-button>
                       </div>
                     </div>
                     <div class="prod-cat" style="font-size: 10px">
-                      <span>Dry</span>
+                      <span>{{ el.product.category }}</span>
                     </div>
                     <div class="prod-price d-flex justify-content-between">
-                      <span>₱100.00</span>
+                      <span>₱{{ frmtd(el.product.price) }}/{{ el.product.unit }}</span>
                       <span class="mx-1">x</span>
-                      <span class="mx-1">1</span>
+                      <span class="mx-1">{{ el.quantity }}</span>
                       <span class="mx-1">=</span>
-                      <span class="mx-1" style="color: #2c9144">₱100.00</span>
+                      <span class="mx-1" style="color: #2c9144">₱{{ frmtd(el.subTotal) }}</span>
                     </div>
                     <div class="product-qty mt-3">
-                      <el-input-number size="mini" v-model="qty"></el-input-number>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="palex-card">
-            <div class="w-100">
-              <div class="store-name ml-3 mb-2 p-2 border-bottom border-secondary">
-                <span> <i class="fas fa-store mr-2"></i> Store 2</span>
-                <!-- <el-divider><i class="el-icon-star-on"></i></el-divider> -->
-              </div>
-              <div class="item border-bottom border-success p-2">
-                <div class="d-flex">
-                  <!-- <div class="check-box mx-3" style="display: flex; align-items: center; height: auto">
-                    <el-button type="danger" icon="el-icon-delete" circle size="mini"></el-button>
-                  </div> -->
-                  <div class="prod-img" style="min-width: 90px">
-                    <img style="width: 80px; height: 80px; object-fit: cover" src="/storage/images/products/3jaCqHANMkXd6F2sNpfxGtmVEtqTBWXq1HNke1k4.jpg" alt="" />
-                  </div>
-                  <div class="prod-name w-100">
-                    <div class="d-flex">
-                      <div>
-                        <span>Product Name 2 </span>
-                      </div>
-                      <div class="ml-2 ml-auto">
-                        <el-button type="danger" icon="el-icon-delete" circle size="mini"></el-button>
-                      </div>
-                    </div>
-                    <div class="prod-cat" style="font-size: 10px">
-                      <span>Dry</span>
-                    </div>
-                    <div class="prod-price d-flex justify-content-between">
-                      <span>₱100.00</span>
-                      <span class="mx-1">x</span>
-                      <span class="mx-1">1</span>
-                      <span class="mx-1">=</span>
-                      <span class="mx-1" style="color: #2c9144">₱100.00</span>
-                    </div>
-                    <div class="product-qty mt-3">
-                      <el-input-number size="mini" v-model="qty"></el-input-number>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="item border-bottom border-success p-2">
-                <div class="d-flex">
-                  <!-- <div class="check-box mx-3" style="display: flex; align-items: center; height: auto">
-                    <el-button type="danger" icon="el-icon-delete" circle size="mini"></el-button>
-                  </div> -->
-                  <div class="prod-img" style="min-width: 90px">
-                    <img style="width: 80px; height: 80px; object-fit: cover" src="/storage/images/products/3jaCqHANMkXd6F2sNpfxGtmVEtqTBWXq1HNke1k4.jpg" alt="" />
-                  </div>
-                  <div class="prod-name w-100">
-                    <div class="d-flex">
-                      <div>
-                        <span>Product Name 3 </span>
-                      </div>
-                      <div class="ml-2 ml-auto">
-                        <el-button type="danger" icon="el-icon-delete" circle size="mini"></el-button>
-                      </div>
-                    </div>
-                    <div class="prod-cat" style="font-size: 10px">
-                      <span>Dry</span>
-                    </div>
-                    <div class="prod-price d-flex justify-content-between">
-                      <span>₱100.00</span>
-                      <span class="mx-1">x</span>
-                      <span class="mx-1">1</span>
-                      <span class="mx-1">=</span>
-                      <span class="mx-1" style="color: #2c9144">₱100.00</span>
-                    </div>
-                    <div class="product-qty mt-3">
-                      <el-input-number size="mini" v-model="qty"></el-input-number>
+                      <el-input-number size="mini" v-model="el.quantity"></el-input-number>
                     </div>
                   </div>
                 </div>
@@ -136,16 +62,16 @@
               <h3 class="my-3">Order Summary</h3>
               <div class="d-flex justify-content-between my-2">
                 <span>Total Order Amount: </span>
-                <span> ₱ 100.00</span>
+                <span> ₱ {{ frmtd(total_order_amount) }}</span>
               </div>
               <div class="d-flex justify-content-between my-2">
                 <span>Discount:</span>
-                <span> ₱ 0.00</span>
+                <span> ₱ {{ frmtd(discount) }}</span>
               </div>
               <hr />
               <div class="d-flex justify-content-between my-2 total">
                 <span><b>Total:</b></span>
-                <span> ₱ 100.00</span>
+                <span> ₱ {{ frmtd(overall_total) }}</span>
               </div>
             </div>
           </div>
@@ -159,9 +85,19 @@ export default {
   props: ["is_auth"],
   data() {
     return {
-      cartList: [{ test: 1 }],
+      cartList: [],
+      //   cartList: [
+      //     {
+      //       items: [
+      //         {
+      //           product: {},
+      //         },
+      //       ],
+      //       vendor: {},
+      //     },
+      //   ],
       user: {},
-      total_order_amout: 0,
+      total_order_amount: 0,
       discount: 0,
       overall_total: 0,
       qty: 1,
@@ -169,12 +105,34 @@ export default {
   },
   mounted() {
     this.getCurrentAuth();
+    this.getCart();
   },
   methods: {
     getCurrentAuth() {
       axios.get("/me").then((res) => {
         this.user = res.data;
       });
+    },
+    getCart() {
+      axios
+        .get(`/getCart`)
+        .then((res) => {
+          this.cartList = res.data.details;
+          this.total_order_amount = res.data.total_order_amount;
+          this.discount = res.data.discount;
+          this.overall_total = res.data.over_all_total;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    frmtd(value) {
+      var n = parseFloat(value).toFixed(2);
+      var withCommas = Number(n).toLocaleString("en", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+      return withCommas;
     },
   },
 };
