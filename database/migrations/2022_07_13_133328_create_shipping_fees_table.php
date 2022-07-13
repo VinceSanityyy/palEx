2a1e661187844ff52e5941989430bb7c5d5b7e00
AbingPj <pj.abing@gmail.com>
2022-07-13 22:52:16 +0800

    Added Shipping Fee Model and migration

---
+++ database/migrations/2022_07_13_133328_create_shipping_fees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShippingFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipping_fees', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('vendor_id')->unsigned();
+            $table->double('shipping_fee_amount', 24, 2)->nullable();
+            $table->timestamps();
+            $table->foreign('vendor_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipping_fees');
+    }
+}
