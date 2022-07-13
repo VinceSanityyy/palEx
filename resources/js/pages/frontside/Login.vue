3f39376324927524e7ab4f0f738a5eaae2b9e601
AbingPj <pj.abing@gmail.com>
2022-07-13 22:48:37 +0800

    Update Login Page

--- resources/js/pages/frontside/Login.vue
+++ resources/js/pages/frontside/Login.vue
@@ -1,31 +1,46 @@
 <template>
   <div class="login-box">
-  <!-- /.login-logo -->
-  <div class="login-logo">
-    <a href="/"><img src="/img/logo/palex3.png" style="width: 100px; height: auto" alt="" /></a>
-  </div>
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Login Here</p>
-      <form @submit.prevent="login" method="post">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email" v-model="email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+    <!-- /.login-logo -->
+    <div class="login-logo">
+      <a href="/"
+        ><img
+          src="/img/logo/palex3.png"
+          style="width: 100px; height: auto"
+          alt=""
+      /></a>
+    </div>
+    <div class="card">
+      <div class="card-body login-card-body">
+        <p class="login-box-msg">Login Here</p>
+        <form @submit.prevent="login" method="post">
+          <div class="input-group mb-3">
+            <input
+              type="email"
+              class="form-control"
+              placeholder="Email"
+              v-model="email"
+            />
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password" v-model="password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <input
+              :type="showpass ? 'text' : 'password'"
+              class="form-control"
+              placeholder="Password"
+              v-model="password"
+            />
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span @click="hideShowPass()" class="fas fa-lock"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <!-- <div class="col-8">
+          <div class="row">
+            <!-- <div class="col-8">
             <div class="icheck-primary">
               <input type="checkbox" id="remember">
               <label for="remember">
@@ -33,15 +48,17 @@
               </label>
             </div>
           </div> -->
-          <!-- /.col -->
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            <!-- /.col -->
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary btn-block">
+                Sign In
+              </button>
+            </div>
+            <!-- /.col -->
           </div>
-          <!-- /.col -->
-        </div>
-      </form>
+        </form>
 
-      <!-- <div class="social-auth-links text-center mb-3">
+        <!-- <div class="social-auth-links text-center mb-3">
         <p>- OR -</p>
         <a href="#" class="btn btn-block btn-primary">
           <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
@@ -50,70 +67,76 @@
           <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
         </a>
       </div> -->
-      <!-- /.social-auth-links -->
+        <!-- /.social-auth-links -->
 
-      <!-- <p class="mb-1">
+        <!-- <p class="mb-1">
         <a href="forgot-password.html">I forgot my password</a>
       </p> -->
-      <p class="mb-0">
-        <a href="/register" class="text-center">Register Here</a>
-      </p>
+        <p class="mb-0">
+          <a href="/register" class="text-center">Register Here</a>
+        </p>
+      </div>
+      <!-- /.login-card-body -->
     </div>
-    <!-- /.login-card-body -->
   </div>
-</div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            email:'',
-            password:'',
-            me:[]
-        }
+  data() {
+    return {
+      email: "",
+      password: "",
+      me: [],
+      showpass: false,
+    };
+  },
+  methods: {
+    hideShowPass() {
+      this.showpass = !this.showpass;
     },
-    methods:{
-        login(){
-            let loader = this.$loading.show({
-                container: this.fullPage ? null : this.$refs.formContainer,
-                onCancel: this.onCancel,
-                color: '#c91010',
-                loader: 'bars',
-                width: 80,
-                height: 100,
-            })
-            axios.post('/login',{
-                email: this.email,
-                password: this.password
-            }).then((res)=>{
-                this.getCurrentAuth()
-            }).catch((err)=>{
-                loader.hide();
-                console.log(err)
-                // toastr.error(err.data)
-                toastr.error(err.response.data.message)
-            })
-        },
-      getCurrentAuth(){
-        axios.get('/me').then((res)=>{
-          console.log(res.data)
-          if(res.data.role == 0){
-            window.location.href = '/admin'
-          }else if (res.data.role == 1){
-            window.location.href = '/vendor'
-          }else{
-            window.location.href = '/customer'
-          }
+    login() {
+      let loader = this.$loading.show({
+        container: this.fullPage ? null : this.$refs.formContainer,
+        onCancel: this.onCancel,
+        color: "#c91010",
+        loader: "bars",
+        width: 80,
+        height: 100,
+      });
+      axios
+        .post("/login", {
+          email: this.email,
+          password: this.password,
         })
-      },
+        .then((res) => {
+          this.getCurrentAuth();
+        })
+        .catch((err) => {
+          loader.hide();
+          console.log(err);
+          // toastr.error(err.data)
+          toastr.error(err.response.data.message);
+        });
     },
-    created(){
-        console.log('Created')
-    }
-}
+    getCurrentAuth() {
+      axios.get("/me").then((res) => {
+        console.log(res.data);
+        if (res.data.role == 0) {
+          window.location.href = "/admin";
+        } else if (res.data.role == 1) {
+          window.location.href = "/vendor";
+        } else {
+          window.location.href = "/customer";
+        }
+      });
+    },
+  },
+  created() {
+    console.log("Created");
+  },
+};
 </script>
 
 <style>
-
 </style>
