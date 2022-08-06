11cc1998e96895ea0390c524461228efd5ad988b
AbingPj <pj.abing@gmail.com>
2022-08-06 19:45:32 +0800

    update chat created time display

--- app/Models/ConversationReply.php
+++ app/Models/ConversationReply.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ConversationReply extends Model
 {
     use HasFactory;
+    protected $casts = [
+        'created_at' => 'datetime:[g:i A] M.d,Y',
+    ];
 }
