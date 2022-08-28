36c5cd482c2039f0055e8186017f8cef898da52a
Ronald Vincent Bustillo <38340124+VinceSanityyy@users.noreply.github.com>
2022-08-28 19:55:33 +0800

    seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(UserSeeder::class);
-        $this->call(VendorProductsSeeder::class);
+        // $this->call(VendorProductsSeeder::class);
     }
 }
